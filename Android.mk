424fa913256c9a62aba6a08f8a00da3f723f0e78
peterzhang68 <zhang.qiwei@serd.sharp-world.com>
2018-04-12 10:02:44 +0800

    modified Android.mk

--- Android.mk
+++ Android.mk
@@ -2,6 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
+#added my comment 2018/4/12
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 	 
 LOCAL_JAVA_LIBRARIES := \
